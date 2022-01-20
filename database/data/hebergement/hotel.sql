c3c103bac2d990e045819e684e58cf40befd9c1e
Arthaudcom <arthaudmorin@gmail.com>
2022-01-20 10:31:58 +0100

    EDIT TABLE HOTEL

--- database/data/hebergement/hotel.sql
+++ database/data/hebergement/hotel.sql
@@ -1,6 +1,6 @@
 INSERT INTO hotel (nom,adresse,nbetoiles,"url",idutilisateur)
 VALUES
-  ('Cras Corporation','776-1802 Egestas St.',3,'url',39),
-  ('Eu Neque Inc.','214-1172 Bibendum Road',4,'url',39),
-  ('Dis Corporation','804-4460 Venenatis Street',1,'url',39),
-  ('Id Magna Industries','218-2367 Arcu. Av.',4,'url',39);
+  ('Cras Corporation','776-1802 Egestas St.',3,'https://fr.mamashelter.com/lyon/chambres/',39),
+  ('Eu Neque Inc.','214-1172 Bibendum Road',4,'https://fr.mamashelter.com/lyon/chambres/',39),
+  ('Dis Corporation','804-4460 Venenatis Street',1,'https://fr.mamashelter.com/lyon/chambres/',39),
+  ('Id Magna Industries','218-2367 Arcu. Av.',4,'https://fr.mamashelter.com/lyon/chambres/',39);
